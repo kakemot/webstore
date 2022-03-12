9d3e18fb098f04de8323125e691e891cade2f15b
Tony Arntsen <tarntsen@enua.no>
2022-03-12 22:54:06 +0100

    init commit

---
+++ webstore/Data/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace webstore.Data
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+    }
+}
