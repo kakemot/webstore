add885b815ac53e6b4e997e8eed60ce6069eb7fb
Tony Arntsen <tarntsen@enua.no>
2022-03-13 02:23:59 +0100

    Some changes

--- webstore/Program.cs
+++ webstore/Program.cs
@@ -8,11 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders =
-        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-});
+
 
 var app = builder.Build();
 
@@ -23,7 +19,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseForwardedHeaders();
 //app.UseHttpsRedirection();
 
 app.UseStaticFiles();
